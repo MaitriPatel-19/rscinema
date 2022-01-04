7c408ad8073a7cd4e10159ba02171f0f322d3267
PurviThakkar11 <impurvithakkar@gmail.com>
2022-01-04 12:38:25 +0530

    index

--- New folder/assets/js/plugins.js
+++ New folder/assets/js/plugins.js
@@ -346,7 +346,7 @@ if(n.refresh(),e.fn.api!==i)if(t=C.api("get request"),o=C.api("was cancelled"))n
                 asNavFor: null,
                 prevArrow: '<button type="button" data-role="none" class="slick-prev" aria-label="Previous" tabindex="0" role="button">Previous</button>',
                 nextArrow: '<button type="button" data-role="none" class="slick-next" aria-label="Next" tabindex="0" role="button">Next</button>',
-                autoplay: false,
+                autoplay: true,
                 autoplaySpeed: 3000,
                 centerMode: false,
                 centerPadding: '50px',
